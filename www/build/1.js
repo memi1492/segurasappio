e50e1b3d7a1fd28493caca85535aa89281e9d1e2
memi1492 <amy.fachman@hotmail.com>
2018-11-23 17:34:35 -0800

    ipad3

--- www/build/1.js
+++ www/build/1.js
@@ -72,7 +72,7 @@ var VintrafPage = /** @class */ (function () {
     };
     VintrafPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-vintraf',template:/*ion-inline-start:"/Users/cloudcen/Documents/GitHub/segurasappio/src/pages/vintraf/vintraf.html"*/'<!--\n  Generated template for the VintrafPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>Vintraf</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n\n</ion-content>\n'/*ion-inline-end:"/Users/cloudcen/Documents/GitHub/segurasappio/src/pages/vintraf/vintraf.html"*/,
+            selector: 'page-vintraf',template:/*ion-inline-start:"C:\Users\Amy\Desktop\segurasappio\src\pages\vintraf\vintraf.html"*/'<!--\n  Generated template for the VintrafPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>Vintraf</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n\n</ion-content>\n'/*ion-inline-end:"C:\Users\Amy\Desktop\segurasappio\src\pages\vintraf\vintraf.html"*/,
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["h" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["i" /* NavParams */]])
     ], VintrafPage);
